290067d3bcf778dea98c8b25297f64bd810c31d7
laugeeme <registro.laura@gmail.com>
2020-10-02 17:41:23 +0200

    Mi aplicacion Django Girls, primer commit

---
+++ intro.py
@@ -0,0 +1,16 @@
+#Esto es un comentario
+
+def hello():
+    print('Hi there!')
+    print('How are you?')
+
+hello()
+
+# Bucles
+def hi(name):
+    print('Hi ' + name + '!')
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+    hi(name)
+    print('Next girl')
